5d0d5f09280c7c666399144e94f86070f1e4af5e
BY-ee <gjwjqgh3@naver.com>
2024-08-06 09:17:41 +0900

    파일 추가

---
+++ src/main/java/com/example/demo/model/Post.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    private Long id;
+    private String title;
+    private String content;
+
+    public Post() {
+
+    }
+
+    public Post(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getContent() {
+        return content;
+    }
+}
