15d7670b473572224973dcec00602d3d0f9538c5
BY-ee <gjwjqgh3@naver.com>
2024-08-07 21:00:11 +0900

    update, delete 구현&paging 처리 진행중

--- src/main/java/com/oraclejpa/controller/PostController.java
+++ src/main/java/com/oraclejpa/controller/PostController.java
@@ -3,11 +3,13 @@ package com.oraclejpa.controller;
 import com.oraclejpa.model.Post;
 import com.oraclejpa.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,41 +24,56 @@ public class PostController {
     }
 
     @GetMapping("/write")
-    public String write() {
+    public String writePost() {
         return "post/write";
     }
 
     @PostMapping("/write")
     public String writePost(@ModelAttribute Post post) {
-        postService.save(post);
+        postService.savePost(post);
         return "post/comSave";
     }
 
+//    @GetMapping("/lists")
+//    public String list(Model model) {
+//        List<Post> dataList = postService.findAllByOrderByIdDesc();
+//        model.addAttribute("dataList", dataList);
+//        return "post/lists";
+//    }
+
+    @GetMapping("/lists")
+    public Page<Post> list(@RequestParam(value = "page", defaultValue = "0") int page,
+                           @RequestParam(value = "size", defaultValue = "10") int size) {
+        return postService.getPosts(page, size);
+    }
+
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Post> dataList = postService.findAll();
-        model.addAttribute("dataList", dataList);
+    public String list(@RequestParam("id") Long id, Model model) {
+        Post post = postService.findById(id);
+        model.addAttribute("post", post);
         return "post/list";
     }
 
-    @PostMapping("/list")
-    public String listToIndex() {
-        return "redirect:/";
+    @GetMapping("/update")
+    public String update(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("id", id);
+        return "post/update";
     }
 
-    @PostMapping("/comSave")
-    public String comToIndex() {
-        return "redirect:/";
-    }
-
-    @GetMapping("/delete")
-    public String delete() {
-        return "post/delete";
+    @PostMapping("/update")
+    public String updatePostById(@RequestParam("id") Long id, @ModelAttribute Post post) {
+        postService.updateById(id, post);
+        return "redirect:/lists";
     }
 
     @PostMapping("/delete")
-    public String deletePostAll() {
-        postService.deleteAll();
-        return "post/comDelete";
+    public String deletePostById(@RequestParam("id") Long id) {
+        postService.deleteById(id);
+        return "redirect:/lists";
+    }
+
+    @GetMapping("/home")
+    public String redirectToHome() {
+        return "redirect:/";
     }
 }
--- src/main/java/com/oraclejpa/repository/PostRepository.java
+++ src/main/java/com/oraclejpa/repository/PostRepository.java
@@ -2,8 +2,29 @@ package com.oraclejpa.repository;
 
 import com.oraclejpa.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByIdDesc();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE post SET title=:title,content=:content WHERE id=:id", nativeQuery = true)
+    void updateById(@Param("id") Long id, @Param("title") String title, @Param("content") String content);
+// @Modifying
+// @Transactional
+// @Query(value = "DROP SEQUENCE post_seq", nativeQuery = true)
+// void dropPostSequence();
+//
+// @Modifying
+// @Transactional
+// @Query(value = "CREATE SEQUENCE post_seq START WITH 1 INCREMENT BY 1 NOCACHE", nativeQuery = true)
+// void createPostSequence();
 }
