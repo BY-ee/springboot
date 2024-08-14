c9a812e096e8be8443b8114451409f704564bd2c
BY-ee <gjwjqgh3@naver.com>
2024-08-14 13:56:46 +0900

    Fix: 외래키 제약으로 인해 닉네임 업데이트가 되지 않는 문제 수정

--- src/main/java/com/oraclejpa/service/UserService.java
+++ src/main/java/com/oraclejpa/service/UserService.java
@@ -2,12 +2,14 @@ package com.oraclejpa.service;
 
 import com.oraclejpa.model.Post;
 import com.oraclejpa.model.User;
+import com.oraclejpa.repository.PostRepository;
 import com.oraclejpa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -17,6 +19,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     public void save(User user) {
         user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
@@ -35,8 +38,16 @@ public class UserService {
         return userRepository.findByUserId(userId);
     }
 
-    public void updateEmailAndNicknameById(String userId, String email, String nickname) {
-        userRepository.updateEmailAndNicknameByUserId(userId, email, nickname);
+//    public void updateEmailAndNicknameById(String userId, String email, String nickname) {
+//        userRepository.updateEmailAndNicknameByUserId(userId, email, nickname);
+//    }
+
+    @Transactional
+    public void updateNicknameForUniqueKeyAndForeignKey(String currentNickname, String email, String nickname) {
+        postRepository.dropConstraint();
+        userRepository.updateEmailAndNicknameByCurrentNickname(currentNickname, email, nickname);
+        postRepository.updateNicknameByCurrentNickname(currentNickname, nickname);
+        postRepository.addConstraint();
     }
 
     public User findUserByPassword(String password) {
